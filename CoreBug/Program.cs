8bc9b6857aceb2ffeea17c09e6c41f3ab507237b
developer <none@none>
2019-11-24 12:36:04 +0200

    init

---
+++ CoreBug/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Buffers;
+
+namespace CoreBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = SpanJson.JsonSerializer.NonGeneric.Utf8.SerializeToArrayPool(new object[] { 1 });
+            ArrayPool<byte>.Shared.Return(buffer.Array);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SpanJson/IJsonFormatter.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SpanJson
+{
+    public interface IJsonFormatter
+    {
+    }
+
+    public interface ICustomJsonFormatter
+    {
+        object Arguments { get; set; }
+    }
+
+    public interface ICustomJsonFormatter<T> : IJsonFormatter<T, byte>, IJsonFormatter<T, char>, ICustomJsonFormatter
+    {
+    }
+
+    public interface IJsonFormatter<T, TSymbol> : IJsonFormatter where TSymbol : struct
+    {
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        void Serialize(ref JsonWriter<TSymbol> writer, T value);
+        T Deserialize(ref JsonReader<TSymbol> reader);
+    }
+}
